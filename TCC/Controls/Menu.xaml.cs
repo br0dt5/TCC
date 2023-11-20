187c97e2c4e34775708427dca4ad030cabfbb524
LucasGMartins <110492355+NecroDruidMetal@users.noreply.github.com>
2023-11-20 14:27:26 -0300

    Update Menu.xaml.cs

--- TCC/Controls/Menu.xaml.cs
+++ TCC/Controls/Menu.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +13,67 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+using NAudio.Wave;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
 namespace Synthesizer.Controls
 {
-    /// <summary>
-    /// Interação lógica para Menu.xam
-    /// </summary>
     public partial class Menu : UserControl
     {
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void MenuItemAbrir_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivos de Áudio|*.wav;*.mp3;*.midi|Todos os Arquivos|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string caminhoDoArquivo = openFileDialog.FileName;
+                // Implement the logic to load the file
+                try
+                {
+                    using (var waveReader = new WaveFileReader(caminhoDoArquivo))
+                    {
+                        // Here you can handle the logic for processing the audio file
+                        MessageBox.Show($"Arquivo de áudio aberto: {caminhoDoArquivo}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao abrir o arquivo: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void MenuItemSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Arquivos WAV|*.wav|Arquivos MP3|*.mp3|Arquivos MIDI|*.midi|Todos os Arquivos|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string caminhoDoArquivo = saveFileDialog.FileName;
+                // Implement logic to save the file
+                try
+                {
+                    using (var waveWriter = new WaveFileWriter(caminhoDoArquivo, new WaveFormat()))
+                    {
+                        // Here you can handle the logic for saving the audio file
+                        MessageBox.Show($"Arquivo de áudio salvo em: {caminhoDoArquivo}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao salvar o arquivo: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
