10139cf07e200133e58324fe911a07c89b3c7b79
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-11-20 16:16:01 -0300

    feature(effects/lfo): created lfos and effects providers

---
+++ TCC/core/LfoProvider.cs
@@ -0,0 +1,64 @@
+﻿using NAudio.Wave;
+using NAudio.Dsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Synthesizer.ViewModel;
+
+namespace Synthesizer.core
+{
+    public class LfoProvider : ISampleProvider
+    {
+        private readonly WaveFormat waveFormat;
+        private readonly string selectedLfoType;
+        private readonly double amount;
+        private readonly double frequency;
+        private double phase;
+        private const double TwoPi = 2 * Math.PI;
+        private ISampleProvider source;
+
+        public LfoProvider(ISampleProvider source, LfoViewModel viewModel)
+        {
+            this.source = source;
+            this.waveFormat = source.WaveFormat;
+            selectedLfoType = viewModel.SelectedLfoType;
+            amount = viewModel.Amount;
+            frequency = viewModel.Frequency;
+        }
+
+        public WaveFormat WaveFormat => waveFormat;
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+
+            int samplesRead = source.Read(buffer, offset, count);
+
+            for (int i = 0; i < samplesRead; i++)
+            {
+                double lfoValue = 0;
+                switch (selectedLfoType)
+                {
+                    case "Senoidal":
+                        lfoValue = Math.Sin(phase) * amount;
+                        break;
+                    case "Quadrada":
+                        lfoValue = Math.Sign(Math.Sin(phase)) * amount;
+                        break;
+                    case "Triangular":
+                        lfoValue = 2 * amount * (2 * Math.Abs((phase / TwoPi) % 1) - 1);
+                        break;
+                }
+
+                buffer[i + offset] *= (float)(1 + lfoValue);
+
+                phase += TwoPi * frequency / waveFormat.SampleRate;
+                if (phase > TwoPi)
+                    phase -= TwoPi;
+            }
+
+            return samplesRead;
+        }
+    }
+}
